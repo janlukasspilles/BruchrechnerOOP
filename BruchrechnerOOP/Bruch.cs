d69971865283b87b0d92cc9d6e6f40cf4feb13a6
janlukas.s <janlukas.s@SP91-I010-21524>
2020-08-26 10:49:59 +0200

    bruchrechner oop grundstruktur

---
+++ BruchrechnerOOP/Bruch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BruchrechnerOOP
+{
+    public class Bruch
+    {
+        #region Attributes
+        private int _zaehler;
+        private int _nenner;
+        #endregion
+
+        #region Properties
+        public int Zaehler { get; set; }
+        public int Nenner { get; set; }
+        #endregion
+
+        #region Constructors
+
+        public Bruch()
+        {
+            this.Zaehler = 1;
+            this.Nenner = 1;
+        }
+
+        public Bruch(int zaehler, int nenner)
+        {
+            this.Zaehler = zaehler;
+            this.Nenner = nenner;
+        }
+
+        #endregion
+
+        #region Methods
+        #endregion
+    }
+}
