bcd68bb5c0c80a1bde8894ec15792349972efdad
Jan-Lukas Spilles <jan-lukasspilles@web.de>
2020-09-21 21:55:42 +0200

    Finale Abgabe

--- BruchrechnerOOP/Controller/Controller.cs
+++ BruchrechnerOOP/Controller/Controller.cs
@@ -1,24 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//Autor:        Jan-Lukas Spilles
+//Klasse:       IA119
+//Datei:        Controller.cs
+//Datum:        16.09.2020
+//Beschreibung: Controllerklasse dieser objektorientierten Anwendung. Steuert den Programmfluss.
+//Aenderungen:  16.09.2020 Erstellung
+//              21.09.2020 Splashinfo, Run-Methode, EvaluateUserEingabe
+using System;
+using System.Threading;
 
 namespace BruchrechnerOOP
 {
     public class Controller
     {
         #region Attributes
+        private UserInterface _userInterface;
         private Bruch _bruch1;
         private Bruch _bruch2;
         private Bruch _ergebnis;
-        private UserInterface _userInterface;
         #endregion
         #region Properties
+        public UserInterface UserInterface { get => _userInterface; set => _userInterface = value; }
         public Bruch Bruch1 { get => _bruch1; set => _bruch1 = value; }
         public Bruch Bruch2 { get => _bruch2; set => _bruch2 = value; }
         public Bruch Ergebnis { get => _ergebnis; set => _ergebnis = value; }
-        public UserInterface UserInterface { get => _userInterface; set => _userInterface = value; }
         #endregion
         #region Constructors
         public Controller()
@@ -33,42 +37,113 @@ namespace BruchrechnerOOP
         public void Run()
         {
             bool isActive = true;
+            Splashinfo();
             do
             {
-                UserInterface.Text = "Bitte geben Sie den ersten Bruch ein:";
-                UserInterface.TextAusgeben();
-                UserInterface.Bruch.Zuweisen(UserInterface.BruchEinlesen());
-                Bruch1.Zuweisen(UserInterface.Bruch);
-
-                UserInterface.Text = "Bitte geben Sie den zweiten Bruch ein:";
-                UserInterface.TextAusgeben();
-                UserInterface.Bruch.Zuweisen(UserInterface.BruchEinlesen());
-                Bruch2.Zuweisen(UserInterface.Bruch);
-
-                UserInterface.Text = "Bitte geben Sie einen der folgenden Operanden ein: + - x /";
-                UserInterface.TextAusgeben();
-                UserInterface.Text = UserInterface.TextEinlesen();
-                switch (UserInterface.Text)
-                {
-                    case "+":
-                        Ergebnis.Zuweisen(Bruch1.Addition(Bruch2));
-                        break;
-                    case "-":
-                        Ergebnis.Zuweisen(Bruch1.Subtraktion(Bruch2));
-                        break;
-                    case "x":
-                        Ergebnis.Zuweisen(Bruch1.Multiplikation(Bruch2));
-                        break;
-                    case "/":
-                        Ergebnis.Zuweisen(Bruch1.Division(Bruch2));
-                        break;
-                    default:
-                        break;
-                }
-                UserInterface.Bruch.Zuweisen(Ergebnis);
-                UserInterface.BruchAusgeben();
+                UserInterface.UpdateUi();
+                EvaluateUserEingabe(UserInterface.UserEingabeEinlesen());
             } while (isActive);
+        }
+
+        private void EvaluateUserEingabe(ConsoleKeyInfo c)
+        {
+            if (c.Key == ConsoleKey.RightArrow)
+            {
+                if (UserInterface.CurrentElement <= 3)
+                    UserInterface.CurrentElement++;
+            }
+            else if (c.Key == ConsoleKey.LeftArrow)
+            {
+                if (UserInterface.CurrentElement >= 1)
+                    UserInterface.CurrentElement--;
+            }
+            else if (c.Key == ConsoleKey.Backspace)
+            {
+                UserInterface.Rechnung[UserInterface.CurrentElement] = UserInterface.Rechnung[UserInterface.CurrentElement].Substring(0, UserInterface.Rechnung[UserInterface.CurrentElement].Length > 0 ? UserInterface.Rechnung[UserInterface.CurrentElement].Length - 1 : 0);
+            }
+            else if (c.Key == ConsoleKey.Enter)
+            {
+                try
+                {
+                    Bruch1.Zuweisen(new Bruch(Convert.ToInt32(UserInterface.Rechnung[0]), Convert.ToInt32(UserInterface.Rechnung[1])));
+                    Bruch2.Zuweisen(new Bruch(Convert.ToInt32(UserInterface.Rechnung[3]), Convert.ToInt32(UserInterface.Rechnung[4])));
+
+                    switch (UserInterface.Rechnung[2])
+                    {
+                        case "+":
+                            Ergebnis.Zuweisen(Bruch1.Addition(Bruch2));
+                            break;
+                        case "-":
+                            Ergebnis.Zuweisen(Bruch1.Subtraktion(Bruch2));
+                            break;
+                        case "x":
+                            Ergebnis.Zuweisen(Bruch1.Multiplikation(Bruch2));
+                            break;
+                        case "/":
+                            Ergebnis.Zuweisen(Bruch1.Division(Bruch2));
+                            break;
+                        default:
+                            throw new ArgumentException("Ungültige Rechenoperation. Unbekannter Operator!");
+                    }
+                    UserInterface.Rechnung[5] = Ergebnis.Zaehler.ToString();
+                    UserInterface.Rechnung[6] = Ergebnis.Nenner.ToString();
+                }
+                catch (Exception e)
+                {
+                    if (e is DivideByZeroException || e is ArgumentException || e is FormatException || e is OverflowException)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine(e.Message);
+                    }
+                    else
+                    {
+                        Console.WriteLine(e.Message + e.StackTrace);
+                    }
+                    Console.ReadKey(true);
+                }
+            }
+            else
+            {
+                if (UserInterface.CurrentElement != 2)
+                {
+                    if (char.IsDigit(c.KeyChar))
+                    {
+                        UserInterface.Rechnung[UserInterface.CurrentElement] += c.KeyChar;
+                    }
+                    else if (UserInterface.Rechnung[UserInterface.CurrentElement].Length == 0 && c.KeyChar == '-')
+                    {
+                        UserInterface.Rechnung[UserInterface.CurrentElement] += c.KeyChar;
+                    }
+                }
+                else if (UserInterface.CurrentElement == 2)
+                {
+                    if (c.KeyChar == '+' || c.KeyChar == '-' || c.KeyChar == 'x' || c.KeyChar == '/')
+                    {
+                        UserInterface.Rechnung[UserInterface.CurrentElement] = c.KeyChar.ToString();
+                    }
+                }
+                else
+                {
+                    //Nichts
+                }
+            }
+        }
+        private void Splashinfo()
+        {
+            string[] titles = { "Projektname:", "Version:", "Datum:", "Autor:", "Klasse:" };
+            string[] information = { "BruchrechnerOOP", "1.3", "21.09.2020", "Jan-Lukas Spilles", "IA119" };
+            Console.CursorTop = 5;
+            for (int i = 0; i < information.Length; i++)
+            {
+                Console.CursorLeft = (Console.WindowWidth - 32) / 2;
+                Console.WriteLine("{0,-12}{1,20}", titles[i], information[i]);
+                Thread.Sleep(400);
+            }
+            Console.SetCursorPosition((Console.WindowWidth - 50) / 2, Console.WindowHeight - 2);
+            Console.WriteLine("Drücken Sie eine beliebige Taste, um fortzufahren.");
         }
         #endregion
     }
+
 }
+
--- BruchrechnerOOP/View/UserInterface.cs
+++ BruchrechnerOOP/View/UserInterface.cs
@@ -1,58 +1,105 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//Autor:        Jan-Lukas Spilles
+//Klasse:       IA119
+//Datei:        UserInterface.cs
+//Datum:        19.09.2020
+//Beschreibung: Viewklasse dieser objektorientierten Anwendung. Nimmt die Daten die Users entgegen und stellt Daten dem User visuell zur Verfügung.
+//Aenderungen:  19.09.2020 Erstellung
+//              21.09.2020 UpdateUi(), UserEingabeEinlesen() und GetLongerLength() implementiert.
+
+using System;
 
 namespace BruchrechnerOOP
 {
     public class UserInterface
     {
         #region Attributes
-        private Bruch _bruch;
-        private string _text;
+        private string[] _rechnung;
+        private int currentElement;
         #endregion
         #region Properties
-        public Bruch Bruch { get => _bruch; set => _bruch = value; }
-        public string Text { get => _text; set => _text = value; }
+
+        public int CurrentElement { get => currentElement; set => currentElement = value; }
+        public string[] Rechnung { get => _rechnung; set => _rechnung = value; }
         #endregion
         #region Constructors
         //Default
         public UserInterface()
         {
-            Bruch = new Bruch();
-            Text = "";
+            Rechnung = new string[] { "", "", "", "", "", "", "" };
+            currentElement = 0;
         }
         #endregion
         #region Methods
-        public Bruch BruchEinlesen()
-        {
-            int Zaehler;
-            int Nenner;
-            Console.WriteLine("Bitte geben Sie einen Zaehler ein.");
-            Zaehler = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Bitte geben Sie einen Nenner ein.");
-            Nenner = Convert.ToInt32(Console.ReadLine());
-            return new Bruch(Zaehler, Nenner);
-        }
 
-        public void BruchAusgeben()
+        public void UpdateUi()
         {
             Console.Clear();
-            Console.WriteLine(Bruch.Zaehler);
-            Console.WriteLine();
-            Console.WriteLine("─");
-            Console.WriteLine(Bruch.Nenner);
+            int trennStrich1 = GetLongerLength(Rechnung[0], Rechnung[1], 1);
+            int trennStrich2 = GetLongerLength(Rechnung[3], Rechnung[4], 1);
+            int trennStrich3 = GetLongerLength(Rechnung[5], Rechnung[6], 1);
+            int posZ1 = Console.CursorLeft + (trennStrich1- Rechnung[0].Length);
+            int posN1 = Console.CursorLeft + (trennStrich1- Rechnung[1].Length);
+            int posZ2 = posZ1 + Rechnung[0].Length + 3 + (trennStrich2 - Rechnung[3].Length);
+            int posN2 = posN1 + Rechnung[1].Length + 3 + (trennStrich2 - Rechnung[4].Length);
+            int posZ3 = posZ2 + Rechnung[3].Length + 3 + (trennStrich3 - Rechnung[5].Length);
+            int posN3 = posN2 + Rechnung[4].Length + 3 + (trennStrich3 - Rechnung[6].Length);
+
+            Rechnung[2] = Rechnung[2].Length == 0 ? " " : Rechnung[2];
+
+            
+            Console.WriteLine("Bruchrechner");
+
+            Console.SetCursorPosition(posZ1, Console.CursorTop);
+            Console.Write($"{Rechnung[0]}");
+            Console.SetCursorPosition(posZ2, Console.CursorTop);
+            Console.Write(Rechnung[3]);
+            Console.SetCursorPosition(posZ3, Console.CursorTop);
+            Console.Write(Rechnung[5]);
+            Console.Write("\r\n");
+
+            Console.Write($"{new string('─', trennStrich1)}");
+            Console.Write($" {Rechnung[2]} ");
+            Console.Write($"{new string('─', trennStrich2)}");
+            Console.Write($" = ");
+            Console.Write($"{new string('─', trennStrich3)}");
+            Console.Write("\r\n");
+
+            Console.SetCursorPosition(posN1, Console.CursorTop);
+            Console.Write($"{Rechnung[1]}");
+            Console.SetCursorPosition(posN2, Console.CursorTop);
+            Console.Write(Rechnung[4]);
+            Console.SetCursorPosition(posN3, Console.CursorTop);
+            Console.Write(Rechnung[6]);
+
+            switch (currentElement)
+            {
+                case 0:
+                    Console.SetCursorPosition(Rechnung[0].Length, 1);
+                    break;
+                case 1:
+                    Console.SetCursorPosition(Rechnung[1].Length, 3);
+                    break;
+                case 2:
+                    Console.SetCursorPosition(trennStrich1+ 1, 2);
+                    break;
+                case 3:
+                    Console.SetCursorPosition(trennStrich1+ Rechnung[3].Length + 3, 1);
+                    break;
+                case 4:
+                    Console.SetCursorPosition(trennStrich1+ Rechnung[4].Length + 3, 3);
+                    break;
+            }
         }
 
-        public string TextEinlesen()
+        public ConsoleKeyInfo UserEingabeEinlesen()
         {
-            return Console.ReadLine();
+            return Console.ReadKey(true);
         }
 
-        public void TextAusgeben()
+        private int GetLongerLength(string a, string b, int defaultReturn)
         {
-            Console.WriteLine(Text);
+            int length = (a.Length > b.Length) ? a.Length : b.Length;
+            return length <= 0 ? defaultReturn : length;
         }
         #endregion
     }
